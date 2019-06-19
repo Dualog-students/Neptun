929427a80f7a528d1b195d46ee255c3e2de7455e
Magnus <kingstellander@gmail.com>
2019-06-19 16:18:11 +0200

    Add dropdown menu for sorting todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { DatePipe } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SortingPipesPipe } from '../pipes/sorting-pipes.pipe';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { SortingPipesPipe } from '../pipes/sorting-pipes.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
