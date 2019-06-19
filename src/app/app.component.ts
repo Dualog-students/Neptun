8fb345f423c6d1db1989511b19d32006bcd869b1
joaalslie <jal029@uit.no>
2019-06-19 15:31:20 +0200

    move home page to it's own home page component, add about page and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { TodoService } from './todo.service'
-import { TodoModel, priorityType } from './models/todo-model';
-import { DatePipe } from '@angular/common'
-import { SortingPipesPipe } from '../pipes/sorting-pipes.pipe'
 
 @Component({
   selector: 'app-root',
@@ -11,72 +7,7 @@ import { SortingPipesPipe } from '../pipes/sorting-pipes.pipe'
 })
 export class AppComponent {
   title = 'Neptun';
-  todoList: TodoModel[] = [];
-  sortingPipe = new SortingPipesPipe();
-  sorting: string = "dateAddedNewest";
 
-  constructor(private todoService: TodoService) {
-    this.todoList = todoService.getAllTodos();
-  }
+  constructor() { }
 
-  getPriority(todo: TodoModel) {
-    return priorityType[todo.priority];
-  }
-
-  deleteTodo(todo: TodoModel) {
-    this.todoService.deleteTodo(todo);
-    this.todoList = this.todoService.getAllTodos();
-  }
-
-  addTodo(todo: TodoModel) { 
-    this.todoService.addTodo(todo);
-    this.todoList = this.todoService.getAllTodos();
-    this.todoList = this.sortingPipe.transform(this.todoList, this.sorting);
-  }
-
-  testAddTodo() {
-    this.todoService.testAddTodo();
-    this.todoList = this.todoService.getAllTodos();
-    this.todoList = this.sortingPipe.transform(this.todoList, this.sorting);
-  }
-
-  toggleComplete(todo: TodoModel) {
-    this.todoService.toggleCompleted(todo);
-    this.todoList = this.todoService.getAllTodos();
-  }
-
-  editTodo(todo: TodoModel, variable: string, edit: any) {
-    this.todoService.editTodo(todo, variable, edit);
-    this.todoList = this.todoService.getAllTodos();
-  }
-
-  getColorPriority(todo: TodoModel) {
-    if(todo.priority == 0){
-      return 'green';
-    }
-    else if(todo.priority == 1) {
-      return 'yellow';
-    }
-    else {
-      return 'red';
-    }
-  }
-
-  getCompleteColor(todo: TodoModel) {
-    if(todo.isDone == true) {
-      return 'green';
-    }
-    else {
-      return 'red';
-    }
-  }
-
-  getCompleteMessage(todo: TodoModel) {
-    if(todo.isDone == true) {
-      return 'yes';
-    }
-    else {
-      return 'no';
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DatePipe } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SortingPipesPipe } from '../pipes/sorting-pipes.pipe';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SortingPipesPipe
+    SortingPipesPipe,
+    AboutComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
