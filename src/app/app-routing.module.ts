fdd2aae125cb6dbd1edec6768b929b7578b7f9c2
joaalslie <jal029@uit.no>
2019-06-20 09:34:44 +0200

    remove unused imports and unnecessary div

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './components/pages/home/home.component';
 
 const routes: Routes = [
