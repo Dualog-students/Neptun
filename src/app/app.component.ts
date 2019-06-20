23aea4b0ec4a3086a18f1d356b058ba6c5ab4d11
markhilb <mhilbertsen@hotmail.no>
2019-06-20 09:45:05 +0200

    Change method of keeping todo-list uupdated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+<<<<<<< HEAD
+=======
+import { TodoService } from './todo.service'
+import { TodoModel } from './models/todo-model';
+import { DatePipe } from '@angular/common'
+import { SortingPipesPipe } from '../pipes/sorting-pipes.pipe'
+>>>>>>> Change method of keeping todo-list uupdated
 
 @Component({
   selector: 'app-root',
@@ -10,4 +17,37 @@ export class AppComponent {
 
   constructor() { }
 
+<<<<<<< HEAD
+=======
+  updateTodoList() {
+    this.todoList = this.todoService.getAllTodos();
+  }
+
+  toggleComplete(todo: TodoModel) {
+    this.todoService.toggleCompleted(todo);
+    this.updateTodoList();
+  }
+
+  deleteTodo(todo: TodoModel) {
+    this.todoService.deleteTodo(todo);
+    this.updateTodoList(); 
+  }
+
+  addTodo(todo: TodoModel) { 
+    this.todoService.addTodo(todo);
+    this.updateTodoList();
+    this.todoList = this.sortingPipe.transform(this.todoList, this.sorting);
+  }
+
+  testAddTodo() {
+    this.todoService.testAddTodo();
+    this.updateTodoList();
+    this.todoList = this.sortingPipe.transform(this.todoList, this.sorting);
+  }
+
+  editTodo(todo: TodoModel, variable: string, edit: any) {
+    this.todoService.editTodo(todo, variable, edit);
+    this.updateTodoList();
+  }
+>>>>>>> Change method of keeping todo-list uupdated
 }
