6e2f357472aaa73123290066890ff4173c812d1e
Magnus <kingstellander@gmail.com>
2019-06-20 12:52:24 +0200

    Working on add-todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { AddTodoComponent } from './components/pages/add-todo/add-todo.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'addTodo', component: AddTodoComponent},
+
 ];
 
 @NgModule({
