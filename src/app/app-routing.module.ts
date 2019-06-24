15bd76f126cf3622cdad649c92378b67bf6aecfa
Magnus Stellander <35449857+gitstella@users.noreply.github.com>
2019-06-24 09:06:46 +0200

    Feature/edit component (#20)
    Add edit component
    
    - Add getTodo function
    - Removed editTodo and added toggleComplete function instead
    - editTodo had unnecessary functionality at this point
    - Changed out editTodo with toggleCompleted in todo-card-component 🎓

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AddTodoComponent } from './components/pages/add-todo/add-todo.component';
+import { EditComponent } from './components/pages/edit/edit.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'addTodo', component: AddTodoComponent},
+  {path: 'edit', component: EditComponent},
+
 
 ];
 
