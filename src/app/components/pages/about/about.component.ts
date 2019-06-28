6cb5c25e2d4d97a672e5d45dbf2dbc97accf8685
joaalslie <jal029@uit.no>
2019-06-28 09:32:54 +0200

    Fix about-page, number of TODOs, add github-button

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoService} from './../../../todo.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
 
+  get_todo_count() {
+    return this.todoService.get_Todos_Length();
+  }
 }
